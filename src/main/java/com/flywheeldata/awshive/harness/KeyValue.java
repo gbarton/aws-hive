1eeb6b2f10f794ac238632a36e28a16d376a7ef9
Kevin Sitto <ksitto@gmail.com>
2015-07-28 12:23:49 -0400

    Scaffolding for the Key/Value data

--- src/main/java/com/flywheeldata/awshive/harness/KeyValue.java
+++ src/main/java/com/flywheeldata/awshive/harness/KeyValue.java
@@ -7,11 +7,18 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import org.json.simple.JSONStreamAware;
 import org.json.simple.JSONValue;
 
 public class KeyValue implements JSONStreamAware {
+	static final String DEFAULT_OBJ_NAME = "com.flywheeldata.awshive.harness.dummyvalue";
+	static final String DEFAULT_METHOD_NAME = "getDummyData";
+	static final String DEFAULT_API_VERSION = "flywheeldata-java-sdk-0.1";
+
+	Random r = new Random();
+
 	Map<String, Object> pair;
 	List<Value> values;
 
@@ -32,78 +39,85 @@ public class KeyValue implements JSONStreamAware {
 
 	Key generateKey() {
 		Key key = new Key();
-		key.setApiVersion("1");
-		key.setMethodName("2");
-		key.setObjName("3");
-		key.setRequestId(4);
-		key.setRequestTime(5);
+		key.setApiVersion(DEFAULT_API_VERSION);
+		key.setMethodName(DEFAULT_METHOD_NAME);
+		key.setObjName(DEFAULT_OBJ_NAME);
+		key.setRequestId(r.nextLong());
+		key.setRequestTime(r.nextLong());
 
 		return key;
 	}
 
 	Value generateValue() {
 		Value value = new Value();
-		value.foo = "bar";
+		value.put("foo", "bar");
+		value.put("baz", r.nextLong());
 
 		return value;
 	}
 
-	static class Key implements JSONStreamAware {
-		Map<String, Object> data;
-
-		public Key() {
-			data = new HashMap<String, Object>(5);
-		}
-
-		public String getObjName() {
-			return (String) data.get("objName");
-
-		}
-
-		public void setObjName(String objName) {
-			data.put("objName", objName);
-		}
-
-		public String getMethodName() {
-			return (String) data.get("methodName");
-		}
-
-		public void setMethodName(String methodName) {
-			data.put("methodName", methodName);
-		}
-
-		public int getRequestTime() {
-			return (int) data.get("requestTime");
-		}
-
-		public void setRequestTime(int requestTime) {
-			data.put("requestTime", requestTime);
-		}
-
-		public int getRequestId() {
-			return (int) data.get("requestId");
-		}
-
-		public void setRequestId(int requestId) {
-			data.put("requestId", requestId);
-		}
-
-		public String getApiVersion() {
-			return (String) data.get("apiVersion");
-		}
-
-		public void setApiVersion(String apiVersion) {
-			data.put("apiVersion", apiVersion);
-		}
+	static class BaseStore implements JSONStreamAware {
+		Map<String, Object> data = new HashMap<String, Object>();
 
 		@Override
 		public void writeJSONString(Writer arg0) throws IOException {
 			JSONValue.writeJSONString(data, arg0);
 		}
+
+		public void put(String key, Object value) {
+			data.put(key, value);
+		}
+
+		public Object get(String key) {
+			return data.get(key);
+		}
 	}
 
-	static class Value {
-		String foo = "";
+	static class Key extends BaseStore {
+		public String getObjName() {
+			return (String) get("objName");
+
+		}
+
+		public void setObjName(String objName) {
+			put("objName", objName);
+		}
+
+		public String getMethodName() {
+			return (String) get("methodName");
+		}
+
+		public void setMethodName(String methodName) {
+			put("methodName", methodName);
+		}
+
+		public long getRequestTime() {
+			return (long) get("requestTime");
+		}
+
+		public void setRequestTime(long requestTime) {
+			put("requestTime", requestTime);
+		}
+
+		public long getRequestId() {
+			return (long) get("requestId");
+		}
+
+		public void setRequestId(long requestId) {
+			put("requestId", requestId);
+		}
+
+		public String getApiVersion() {
+			return (String) get("apiVersion");
+		}
+
+		public void setApiVersion(String apiVersion) {
+			data.put("apiVersion", apiVersion);
+		}
+	}
+
+	static class Value extends BaseStore {
+
 	}
 
 	@Override
---
+++ src/test/java/com/flywheeldata/awshive/hadoop/JsonKeyArrayValueRecordReaderTest.java
@@ -0,0 +1,5 @@
+package com.flywheeldata.awshive.hadoop;
+
+public class JsonKeyArrayValueRecordReaderTest {
+
+}
